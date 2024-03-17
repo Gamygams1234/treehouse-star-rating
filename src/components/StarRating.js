95da75aa8cf08562c8bee0fca0dc5728c6466cc2
Gamaliel Burgos <54601555+Gamygams1234@users.noreply.github.com>
2024-03-16 19:42:06 -0700

    first commit

---
+++ src/components/StarRating.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import Star from "./Star";
+
+const StarRating = () => {
+  // Initialize a 'courseRating' state
+  const [courseRating, setCourseRating] = useState(0);
+
+  // Write a function that returns 5 Star components
+
+  const selectRating = (event, rating) => {
+    setCourseRating(rating);
+  };
+
+  const renderStars = () => {
+    const stars = [];
+    for (let i = 1; i <= 5; i++) {
+      stars.push(<Star key={i} selected={i <= courseRating} setRating={(event) => selectRating(event, i)} />);
+    }
+    return stars;
+  };
+
+  // Write an event handler that updates the courseRating state.
+  // Pass the function to a Star component via props
+
+  return (
+    <ul className="course--stars" id="course--stars">
+      {/* Render the Star components */}
+      {renderStars()}
+    </ul>
+  );
+};
+
+export default StarRating;
